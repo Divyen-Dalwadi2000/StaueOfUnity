f8f72c8b517bd3337bc493fa97d4edb44675e459
Divyen <dalwadiddj2000@gmail.com>
2024-05-17 18:22:03 +0530

    Remove CDN links for react and add the PARCEL Module

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
 const h1 = React.createElement("h1", { id: "title1" }, "Dalwadi Divyen"); // react Element
 // console.log(h1);  // // react Element is Nothing But the Object 
 const h2 = React.createElement("h2", { id: "title2" }, "Divyen Dalwadi")
